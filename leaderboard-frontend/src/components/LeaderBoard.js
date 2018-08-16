2f5b37c259adca48328c4b8fb9c86f397e047ab2
JasonCorp84 <csababalogh84@gmail.com>
2018-08-15 18:29:12 -0700

    Leaderboard refactored Still need to hook up with toutes on agreement

--- leaderboard-frontend/src/components/LeaderBoard.js
+++ leaderboard-frontend/src/components/LeaderBoard.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './LeaderBoard.css';
-
+import './LeaderboardComponents/ActivityFeed'
+import ActivityFeed from './LeaderboardComponents/ActivityFeed';
+import WeeklyLeaderboard from './LeaderboardComponents/WeeeklyLeaderboard';
+import OverallLeaderboard from './LeaderboardComponents/OverallLeaderboard';
+import HotRightNow from './LeaderboardComponents/HotRightNow';
 
 class LeaderBoard extends Component {
     constructor(props) {
@@ -13,85 +17,18 @@ class LeaderBoard extends Component {
                 { name: "Joe Chan", gitScore: 8, huntrScore: 9, Total: 43 },
                 { name: "Mike Moo", gitScore: 7, huntrScore: 10, Total: 41 },
             ]
-            
          }
     }
     render() { 
         return ( 
             <div className="main-wrapper">
-        <div className="top-row">
-            <div className="top-cohort">CS24</div>
-
-            <div className="top-activityFeed">
-                <h5>Activity Feed</h5> <br/>
-                Jone Doe applied a job! Number 6 this week and 17 total! <br/>
-                John Smith scheduled a phone interview! <br/>
-                Maria Martinez contributed to guthub! <br/>
-                Joe Chan got a job! <br/>
-                Jone Doe applied a job! Number 6 this week and 17 total! <br/>
-                John Smith scheduled a phone interview! <br/>
-            </div>
-
-            <div className="top-hired">54/96</div>
-        </div>
-
-
-        <div className="mid-row">
-            <div className="mid-weekly"> 
-                <h5>Weekly LeaderBoard</h5>
-                <div className="mid-weekly__wrapper">
-                    <div className="mid-title mid-top-title">
-                        <div className="weekly__name">Name</div>
-                        <div className="weekly__rest">Github</div>
-                        <div className="weekly__rest">Huntr</div>
-                        <div className="weekly__rest">Total</div>
-                    </div>
-                    <div className="mid-weekly__containers">
-                        <div className="mid-weekly__nameContainer weekly__name">{
-                           this.state.students.map((student, i) => {
-                                return(
-                                    <div className="mid-title title">
-                                        <div className="weekly__name" key={i}>{student.name}</div>
-                                        <div className="weekly__rest" key={i}>{student.gitScore}</div>
-                                        <div className="weekly__rest"  key={i}>{student.huntrScore}</div>
-                                        <div className="weekly__rest"  key={i}>{student.Total}</div>
-                                    </div>
-                                )
-                           })
-                        }</div>
-                       
-                    </div>
+                <ActivityFeed />
+                <div className="mid-row">
+                    <WeeklyLeaderboard />
+                    <OverallLeaderboard />
                 </div>
+                <HotRightNow />
             </div>
-
-
-            <div className="mid-weekly">
-                 <h5>Overall LeaderBoard</h5>
-                 <div className="mid-weekly__wrapper">
-                    <div className="mid-title">
-                        <div className="weekly__name">Name</div>
-                        <div className="weekly__rest">Github</div>
-                        <div className="weekly__rest">Huntr</div>
-                        <div className="weekly__rest">Total</div>
-                    </div>
-                    <div className="mid-weekly__containers">
-                        <div className="mid-weekly__nameContainer weekly__name">name</div>
-                        <div className="mid-weekly_githubContainer weekly__rest"></div>
-                        <div className="mid-weekly__huntrContainer weekly__rest"></div>
-                        <div className="mid-weekly__totalContainer weekly__rest"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-        <div className="bottom-row">
-            <div className="bottom-github">GITHUBLOGO:  Maria Martinez</div>
-            <div className="bottom-huntr">HUNTR: Joe Chan</div>
-        </div>
-
-
-    </div>
          );
     }
 }
