5225556aa1a02b443acf4fcff9803a22944f2170
Eric Andrade <eric.andrade@gmail.com>
2018-08-24 10:12:08 -0400

    refactored leaderboard and leaderboard components and added styling.

--- leaderboard-frontend/src/components/LeaderboardComponents/OverallLeaderboard.js
+++ leaderboard-frontend/src/components/LeaderboardComponents/OverallLeaderboard.js
@@ -1,68 +1,145 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class OverallLeaderboard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            students: [
-                { name: "Maria Martinez", gitScore: 11, huntrScore: 13, Total: 72 },
-                { name: "Jane Doe", gitScore: 10, huntrScore: 11, Total: 56 },
-                { name: "John Smith", gitScore: 9, huntrScore: 10, Total: 44 },
-                { name: "Joe Chan", gitScore: 8, huntrScore: 9, Total: 43 },
-                { name: "Mike Moo", gitScore: 7, huntrScore: 10, Total: 41 },
-                { name: "Maria Martinez", gitScore: 11, huntrScore: 13, Total: 72 },
-                { name: "Jane Doe", gitScore: 10, huntrScore: 11, Total: 56 },
-                { name: "John Smith", gitScore: 9, huntrScore: 10, Total: 44 },
-                { name: "Joe Chan", gitScore: 8, huntrScore: 9, Total: 43 },
-                { name: "Mike Moo", gitScore: 7, huntrScore: 10, Total: 41 },{ name: "Maria Martinez", gitScore: 11, huntrScore: 13, Total: 72 },
-                { name: "Jane Doe", gitScore: 10, huntrScore: 11, Total: 56 },
-                { name: "John Smith", gitScore: 9, huntrScore: 10, Total: 44 },
-                { name: "Joe Chan", gitScore: 8, huntrScore: 9, Total: 43 },
-                { name: "Mike Moo", gitScore: 7, huntrScore: 10, Total: 41 },{ name: "Maria Martinez", gitScore: 11, huntrScore: 13, Total: 72 },
-                { name: "Jane Doe", gitScore: 10, huntrScore: 11, Total: 56 },
-                { name: "John Smith", gitScore: 9, huntrScore: 10, Total: 44 },
-                { name: "Joe Chan", gitScore: 8, huntrScore: 9, Total: 43 },
-                { name: "Mike Moo", gitScore: 7, huntrScore: 10, Total: 41 },{ name: "Maria Martinez", gitScore: 11, huntrScore: 13, Total: 72 },
-                { name: "Jane Doe", gitScore: 10, huntrScore: 11, Total: 56 },
-                { name: "John Smith", gitScore: 9, huntrScore: 10, Total: 44 },
-                { name: "Joe Chan", gitScore: 8, huntrScore: 9, Total: 43 },
-                { name: "Mike Moo", gitScore: 7, huntrScore: 10, Total: 41 },{ name: "Maria Martinez", gitScore: 11, huntrScore: 13, Total: 72 },
-                { name: "Jane Doe", gitScore: 10, huntrScore: 11, Total: 56 },
-                { name: "John Smith", gitScore: 9, huntrScore: 10, Total: 44 },
-                { name: "Joe Chan", gitScore: 8, huntrScore: 9, Total: 43 },
-                { name: "Mike Moo", gitScore: 7, huntrScore: 10, Total: 41 },
-            ]        
-         }
-    }
-    render() { 
-        return ( 
-            <div className="mid-weekly"> 
-            <h5>Weekly LeaderBoard</h5>
-            <div className="mid-weekly__wrapper">
-                <div className="mid-title mid-top-title">
-                    <div className="mid-weekly__name">Name</div>
-                    <div className="mid-weekly__rest mid-title-name">Github</div>
-                    <div className="mid-weekly__rest mid-title-name">Huntr</div>
-                    <div className="mid-weekly__rest mid-title-name">Total</div>
-                </div>
-                <div className="mid-weekly__containers">
-                    <div className="mid-weekly__nameContainer mid-weekly__name">{
-                       this.state.students.map((student, i) => {
-                            return(
-                                <div className="mid-title title">
-                                    <div className="mid-weekly__name mid-list" key={i}>{student.name}</div>
-                                    <div className="mid-weekly__rest mid-list" key={i}>{student.gitScore}</div>
-                                    <div className="mid-weekly__rest mid-list"  key={i}>{student.huntrScore}</div>
-                                    <div className="mid-weekly__rest mid-list"  key={i}>{student.Total}</div>
-                                </div>
-                            )
-                       })
-                    }</div>            
-                </div>
-            </div>
-        </div>
-         );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      //   students: [
+      //     { name: "Maria Martinez", gitScore: 11, huntrScore: 13, Total: 72 },
+      //     { name: "Jane Doe", gitScore: 10, huntrScore: 11, Total: 56 },
+      //     { name: "John Smith", gitScore: 9, huntrScore: 10, Total: 44 },
+      //     { name: "Joe Chan", gitScore: 8, huntrScore: 9, Total: 43 },
+      //     { name: "Mike Moo", gitScore: 7, huntrScore: 10, Total: 41 },
+      //     { name: "Maria Martinez", gitScore: 11, huntrScore: 13, Total: 72 },
+      //     { name: "Jane Doe", gitScore: 10, huntrScore: 11, Total: 56 },
+      //     { name: "John Smith", gitScore: 9, huntrScore: 10, Total: 44 },
+      //     { name: "Joe Chan", gitScore: 8, huntrScore: 9, Total: 43 },
+      //     { name: "Mike Moo", gitScore: 7, huntrScore: 10, Total: 41 },
+      //     { name: "Maria Martinez", gitScore: 11, huntrScore: 13, Total: 72 },
+      //     { name: "Jane Doe", gitScore: 10, huntrScore: 11, Total: 56 },
+      //     { name: "John Smith", gitScore: 9, huntrScore: 10, Total: 44 },
+      //     { name: "Joe Chan", gitScore: 8, huntrScore: 9, Total: 43 },
+      //     { name: "Mike Moo", gitScore: 7, huntrScore: 10, Total: 41 },
+      //     { name: "Maria Martinez", gitScore: 11, huntrScore: 13, Total: 72 },
+      //     { name: "Jane Doe", gitScore: 10, huntrScore: 11, Total: 56 },
+      //     { name: "John Smith", gitScore: 9, huntrScore: 10, Total: 44 },
+      //     { name: "Joe Chan", gitScore: 8, huntrScore: 9, Total: 43 },
+      //     { name: "Mike Moo", gitScore: 7, huntrScore: 10, Total: 41 },
+      //     { name: "Maria Martinez", gitScore: 11, huntrScore: 13, Total: 72 },
+      //     { name: "Jane Doe", gitScore: 10, huntrScore: 11, Total: 56 },
+      //     { name: "John Smith", gitScore: 9, huntrScore: 10, Total: 44 },
+      //     { name: "Joe Chan", gitScore: 8, huntrScore: 9, Total: 43 },
+      //     { name: "Mike Moo", gitScore: 7, huntrScore: 10, Total: 41 },
+      //     { name: "Maria Martinez", gitScore: 11, huntrScore: 13, Total: 72 },
+      //     { name: "Jane Doe", gitScore: 10, huntrScore: 11, Total: 56 },
+      //     { name: "John Smith", gitScore: 9, huntrScore: 10, Total: 44 },
+      //     { name: "Joe Chan", gitScore: 8, huntrScore: 9, Total: 43 },
+      //     { name: "Mike Moo", gitScore: 7, huntrScore: 10, Total: 41 }
+      //   ]
+    };
+  }
+  render() {
+    return (
+        <Container>
+          <Table color={"grey"} celled inverted selectable>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell colSpan="4">
+                  Overall Leaderboard
+                </Table.HeaderCell>
+              </Table.Row>
+              <Table.Row>
+                <Table.HeaderCell>Name</Table.HeaderCell>
+                <Table.HeaderCell>Github</Table.HeaderCell>
+                <Table.HeaderCell>Huntr</Table.HeaderCell>
+                <Table.HeaderCell>Total</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell>
+                  <Header as="h4" image>
+                    <Image
+                      src="https://avatars3.githubusercontent.com/u/12102923?s=460&v=4"
+                      rounded
+                      size="mini"
+                    />
+                    <Header.Content>Claudio Gonzalez</Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>47</Table.Cell>
+                <Table.Cell>31</Table.Cell>
+                <Table.Cell>78</Table.Cell>
+              </Table.Row>
+
+              <Table.Row>
+                <Table.Cell>
+                  <Header as="h4" image>
+                    <Image
+                      src="https://avatars2.githubusercontent.com/u/115880?s=400&u=ac3673f7e699efa57bf31c66afb2123ce1689b41&v=4"
+                      rounded
+                      size="mini"
+                    />
+                    <Header.Content>Eric Andrade</Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>44</Table.Cell>
+                <Table.Cell>34</Table.Cell>
+                <Table.Cell>78</Table.Cell>
+              </Table.Row>
+
+              <Table.Row>
+                <Table.Cell>
+                  <Header as="h4" image>
+                    <Image
+                      src="https://avatars3.githubusercontent.com/u/7462727?s=460&v=4"
+                      rounded
+                      size="mini"
+                    />
+                    <Header.Content>Aaron Faulkner</Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>50</Table.Cell>
+                <Table.Cell>28</Table.Cell>
+                <Table.Cell>78</Table.Cell>
+              </Table.Row>
+
+              <Table.Row>
+                <Table.Cell>
+                  <Header as="h4" image>
+                    <Image
+                      src="https://avatars0.githubusercontent.com/u/36025407?s=460&v=4"
+                      rounded
+                      size="mini"
+                    />
+                    <Header.Content>Shawn Stewart</Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>46</Table.Cell>
+                <Table.Cell>32</Table.Cell>
+                <Table.Cell>78</Table.Cell>
+              </Table.Row>
+
+              <Table.Row>
+                <Table.Cell>
+                  <Header as="h4" image>
+                    <Image
+                      src="https://avatars3.githubusercontent.com/u/35821558?s=460&v=4"
+                      rounded
+                      size="mini"
+                    />
+                    <Header.Content>Abraham Bueno</Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>44</Table.Cell>
+                <Table.Cell>34</Table.Cell>
+                <Table.Cell>78</Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table>
+        </Container>
+    );
+  }
 }
- 
+
 export default OverallLeaderboard;
