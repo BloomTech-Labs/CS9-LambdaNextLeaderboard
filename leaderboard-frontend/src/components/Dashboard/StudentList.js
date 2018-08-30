3b8fc8f00a2f3c9646dd927b5cbf8535f3355289
Shawn Stewart <s.stewart0220@gmail.com>
2018-08-30 15:12:08 -0400

    Added student cards

--- leaderboard-frontend/src/components/Dashboard/StudentList.js
+++ leaderboard-frontend/src/components/Dashboard/StudentList.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
-import { Segment } from "semantic-ui-react";
+import { Segment, Card } from "semantic-ui-react";
 
 export default class StudentList extends Component {
   render() {
     console.log("STUDNET PROPS", this.props);
-    return <Segment>Hello</Segment>;
+    return (
+      <Segment>
+        <Card.Group itemsPerRow="3">
+          {this.props.students.map(student => {
+            return (
+              <Card>
+                <Card.Content>
+                  <Card.Header>{`${student.firstname} ${
+                    student.lastname
+                  }`}</Card.Header>
+                </Card.Content>
+              </Card>
+            );
+          })}
+        </Card.Group>
+      </Segment>
+    );
   }
 }
