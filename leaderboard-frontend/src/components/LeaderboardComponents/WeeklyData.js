899b863d35513823f69951933f493fde91b741c8
Abraham Bueno <abueno@localhost.localdomain>
2018-08-29 21:35:35 -0500

    Added a catch for unreliable requests returned from github.

--- leaderboard-frontend/src/components/LeaderboardComponents/WeeklyData.js
+++ leaderboard-frontend/src/components/LeaderboardComponents/WeeklyData.js
@@ -1,52 +1,11 @@
 import React, {Component} from 'react';
-import {connectAsync} from 'iguazu';
-import {queryAllMyData, queryGithub, queryStudents} from "../../actions";
 import {Container, Table} from "semantic-ui-react";
 import WeeklyDisplay from "./WeeklyDisplay";
 
 
 class WeeklyData extends Component {
-
     render() {
-        if (this.props.isLoading()) {
-            if (localStorage.getItem("invalid")) {
-                localStorage.removeItem("token");
-                localStorage.removeItem("invalid");
-                this.props.props.props.history.push('/')
-            }
-            return <div>Loading...</div>
-        }
-
-        if (this.props.loadedWithErrors()) {
-            return <div>Oh no! Something went wrong</div>
-        }
-        console.log("We have data", this.props.props.match.params.classname)
         let count = []
-        const studentsObject = [];
-        this.props.students.forEach((each, i) => {
-            if (each.classname === this.props.props.match.params.classname) {
-                studentsObject.push({
-                    each
-                })
-            }
-
-        })
-        const gitObject = [];
-        this.props.data.gitData.forEach((git, x) => {
-            studentsObject.forEach(student => {
-                if (git.FullName === student.each.firstname + ' ' + student.each.lastname) {
-                    gitObject.push(git)
-                }
-            })
-        })
-        if (gitObject.length === 0) {
-            return (
-                <div>
-                    <h1>No Students</h1>
-                </div>
-            )
-        }
-
         return (
             <div>
                 <Container>
@@ -62,7 +21,7 @@ class WeeklyData extends Component {
                                 <Table.HeaderCell>Total</Table.HeaderCell>
                             </Table.Row>
                         </Table.Header>
-                            {gitObject.map((each, x) => {
+                            {this.props.gitObject.map((each, x) => {
                                 this.props.data.huntr.forEach(hunt => {
                                     if (each.FullName === hunt.givenNameArr + ' ' + hunt.familyName) {
                                         count[x] = hunt.count;
@@ -81,17 +40,7 @@ class WeeklyData extends Component {
     }
 }
 
-export function loadDataAsProps({store, ownProps}) {
-    const {dispatch} = store;
-
-    const path = "/"; // Use the actual path when it's created as needed
-    console.log(ownProps);
-    return {
-        // classdata: () => dispatch(queryAllMyData(path)),
-        students: () => dispatch(queryStudents()),
-        data: () => dispatch(queryGithub())
-    };
-}
 
 
-export default connectAsync({loadDataAsProps})(WeeklyData);
+
+export default WeeklyData
