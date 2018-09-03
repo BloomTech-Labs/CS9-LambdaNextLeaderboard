c49c23b034a419953cd876d5ee9c6ea810ecc864
cgoez <goez2001@gmail.com>
2018-09-03 16:40:36 -0700

    Initial landing page rework

--- leaderboard-frontend/src/components/LandingPage/LandingPage.js
+++ leaderboard-frontend/src/components/LandingPage/LandingPage.js
@@ -6,7 +6,10 @@ import {
   Form,
   Input,
   Button,
-  Icon
+  Icon,
+  Segment,
+  GridColumn,
+  Image
 } from "semantic-ui-react";
 
 import DesktopContainer from "./LandingPageComponents/DesktopContainer";
@@ -38,37 +41,95 @@ class LandingPage extends Component {
 
   render() {
     return (
-      <div className="Landing">
-        <div className="Landing__head">
+      <div>
         <Container text>
-    <Header
-      as='h1'
-      content='Next Steps'
-      inverted
-      style={{
-        fontSize:'4em',
-        fontWeight: 'normal',
-        marginBottom: 0,
-        marginTop:'3em',
-      }}
-    />
-    <Header
-      as='h2'
-      content='Easily track student job progress.'
-      inverted
-      style={{
-        fontSize:'1.7em',
-        fontWeight: 'normal',
-        marginTop:'1.5em',
-      }}
-    />
-    <Button primary size='huge'>
-      Get Started
-      <Icon name='right arrow' />
-    </Button>
-  </Container>
-            
-            {/* <Grid columns={2} stackable>
+          <Header
+            as="h1"
+            content="Next Steps"
+            // inverted
+            style={{
+              fontSize: "4em",
+              fontWeight: "normal",
+              marginBottom: 0,
+              marginTop: "3em"
+            }}
+          />
+          <Header
+            as="h2"
+            content="Easily track student job progress."
+            // inverted
+            style={{
+              fontSize: "1.7em",
+              fontWeight: "normal",
+              marginTop: "1.5em"
+            }}
+          />
+          <Button primary size="huge">
+            Get Started
+            <Icon name="right arrow" />
+          </Button>
+        </Container>
+        <Container fluid>
+          <Segment>
+            <Grid container stackable verticalAlign="middle">
+              <Grid.Row>
+                <Grid.Column width={8}>
+                  <Header as="h3" style={{ fontSize: "2em" }}>
+                    Fun and competetive leaderboards.{" "}
+                  </Header>
+                  <p style={{ fontSize: "1.33em" }}>
+                    Do some things in this paragraph that are cool
+                  </p>
+                  <Header as="h3" style={{ fontSize: "2em" }}>
+                    {" "}
+                    Realtime statistics{" "}
+                  </Header>
+                  <p style={{ fontSize: "1.33em" }}>
+                    More thoughtful things to say
+                  </p>
+                </Grid.Column>
+                <Grid.Column floated="right" width={6}>
+                  <Image bordered rounded size="large" src="temp" />
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column textAlign="center">
+                  <Button size="huge">Sign up</Button>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Segment>
+
+          <Segment style={{ padding: "0em" }} vertical>
+            <Grid celled="internally" columns="equal" stackable>
+              <Grid.Row textAlign="center">
+                <Grid.Column
+                  style={{ paddingBottom: "5em", paddingTop: "5em" }}
+                >
+                  <Header as="h3" style={{ fontSize: "2em" }}>
+                    "What a Company"
+                  </Header>
+                  <p style={{ fontSize: "1.33em" }}>
+                    That is what they all say about us
+                  </p>
+                </Grid.Column>
+                <Grid.Column
+                  style={{ paddingBottom: "5em", paddingTop: "5em" }}
+                >
+                  <Header as="h3" style={{ fontSize: "2em" }}>
+                    "I always thought I made too many commits.  Now it's taken me to the top!"
+                  </Header>
+                  <p style={{ fontSize: "1.33em" }}>
+                    <Image avatar src="/images/avatar/large/nan.jpg" />
+                    <b>Nan</b> Github user
+                  </p>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Segment>
+        </Container>
+
+        {/* <Grid columns={2} stackable>
               <Grid.Column verticalAlign="middle">
                 <Header as="h1" size="huge" color="olive">
                   Keep track of work and progress with NextSteps
@@ -125,9 +186,7 @@ class LandingPage extends Component {
                 </Form>
               </Grid.Column>
             </Grid> */}
-          {/* </Container> */}
-        </div>
-        <div className="Landing__content">Content</div>
+        {/* </Container> */}
       </div>
     );
   }
