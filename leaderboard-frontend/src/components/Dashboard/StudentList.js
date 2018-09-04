dd946d5f50c04b42417a70a891fbba39779124b7
Shawn Stewart <s.stewart0220@gmail.com>
2018-09-04 14:46:24 -0400

    Styled student edit.

--- leaderboard-frontend/src/components/Dashboard/StudentList.js
+++ leaderboard-frontend/src/components/Dashboard/StudentList.js
@@ -6,7 +6,8 @@ import {
   List,
   Modal,
   Header,
-  Icon
+  Icon,
+  Input
 } from "semantic-ui-react";
 
 export default class StudentList extends Component {
@@ -15,18 +16,18 @@ export default class StudentList extends Component {
 
     this.state = {
       selectedStudent: "",
-      openEditModal: false,
-      openDeleteModal: false
+      editStudent: false,
+      openDeleteModal: false,
+      updatedInfo: {}
     };
   }
 
-  openModal = (e, { id, name }) => {
-    this.setState({ [name]: !this.state[name], selectedStudent: id });
+  openModal = (e, { id }) => {
+    this.setState({ openDeleteModal: true, selectedStudent: id });
   };
 
   closeModal = () => {
     this.setState({
-      openEditModal: false,
       openDeleteModal: false,
       selectedStudent: ""
     });
@@ -36,11 +37,35 @@ export default class StudentList extends Component {
     this.props.updateStudent({ id: name, hired: true });
   };
 
+  openEditView = (e, { student }) => {
+    let current = {};
+    current.firstname = student.firstname;
+    current.lastname = student.lastname;
+    current.email = student.email;
+    current.github = student.github;
+    this.setState({
+      editStudent: true,
+      selectedStudent: student._id,
+      updatedInfo: current
+    });
+  };
+
+  closeEditView = () => {
+    this.setState({ editStudent: false, selectedStudent: "", updatedInfo: {} });
+  };
+
+  handleInput = (e, { name, value }) => {
+    let current = Object.assign({}, this.state.updatedInfo);
+    current[name] = value;
+    this.setState({ updatedInfo: current });
+  };
+
   handleDelete = () => {
     this.props.deleteStudent({ id: this.state.selectedStudent });
   };
 
   render() {
+    console.log(this.state);
     return (
       <Segment>
         <DeleteModal
@@ -57,52 +82,103 @@ export default class StudentList extends Component {
         <Card.Group itemsPerRow="2" stackable>
           {this.props.students.map((student, index) => {
             return !student.hired ? (
-              <Card key={index}>
-                <Card.Content>
-                  <Card.Header>{`${student.firstname} ${
-                    student.lastname
-                  }`}</Card.Header>
-                  <Card.Description>
-                    <List>
-                      <List.Item>Email Address: {student.email}</List.Item>
-                      <List.Item>Github Handle: {student.github}</List.Item>
-                    </List>
-                  </Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                  <Button.Group widths="3">
-                    <Button
-                      name={student._id}
-                      icon="money"
-                      content="Hired"
-                      inverted
-                      color="green"
-                      size="small"
-                      onClick={this.handleHire}
+              this.state.editStudent &&
+              this.state.selectedStudent === student._id ? (
+                <Card key={index}>
+                  <Card.Content>
+                    <Input
+                      name="firstname"
+                      placeholder="Student first name"
+                      value={this.state.updatedInfo.firstname}
+                      onChange={this.handleInput}
+                      fluid
                     />
-                    <Button
-                      id={student._id}
-                      name="openEditModal"
-                      icon="wrench"
-                      content="Edit"
-                      inverted
-                      color="blue"
-                      size="small"
-                      onClick={this.openModal}
+                    <Input
+                      name="lastname"
+                      placeholder="Student last name"
+                      value={this.state.updatedInfo.lastname}
+                      onChange={this.handleInput}
+                      fluid
                     />
-                    <Button
-                      id={student._id}
-                      name="openDeleteModal"
-                      icon="trash"
-                      content="Delete"
-                      inverted
-                      color="red"
-                      size="small"
-                      onClick={this.openModal}
+                    <Input
+                      name="email"
+                      placeholder="Student email address"
+                      value={this.state.updatedInfo.email}
+                      onChange={this.handleInput}
+                      fluid
                     />
-                  </Button.Group>
-                </Card.Content>
-              </Card>
+                    <Input
+                      name="github"
+                      placeholder="Student Github handle"
+                      value={this.state.updatedInfo.github}
+                      onChange={this.handleInput}
+                      fluid
+                    />
+                  </Card.Content>
+                  <Card.Content extra>
+                    <Button.Group widths="2">
+                      <Button content="Update" color="blue" inverted />
+                      <Button
+                        content="Cancel"
+                        onClick={this.closeEditView}
+                        color="red"
+                        inverted
+                      />
+                    </Button.Group>
+                  </Card.Content>
+                </Card>
+              ) : (
+                <Card key={index}>
+                  <Card.Content>
+                    <Card.Header>{`${student.firstname} ${
+                      student.lastname
+                    }`}</Card.Header>
+                    <Card.Description>
+                      <List>
+                        <List.Item>Email Address: {student.email}</List.Item>
+                        <List.Item>Github Handle: {student.github}</List.Item>
+                      </List>
+                    </Card.Description>
+                  </Card.Content>
+                  <Card.Content extra>
+                    <Button.Group widths="3">
+                      <Button
+                        name={student._id}
+                        icon="money"
+                        content="Hired"
+                        inverted
+                        color="green"
+                        size="small"
+                        onClick={this.handleHire}
+                        disabled={this.state.editStudent}
+                      />
+                      <Button
+                        id={student._id}
+                        student={student}
+                        name="openEditModal"
+                        icon="wrench"
+                        content="Edit"
+                        inverted
+                        color="blue"
+                        size="small"
+                        onClick={this.openEditView}
+                        disabled={this.state.editStudent}
+                      />
+                      <Button
+                        id={student._id}
+                        name="openDeleteModal"
+                        icon="trash"
+                        content="Delete"
+                        inverted
+                        color="red"
+                        size="small"
+                        onClick={this.openModal}
+                        disabled={this.state.editStudent}
+                      />
+                    </Button.Group>
+                  </Card.Content>
+                </Card>
+              )
             ) : null;
           })}
         </Card.Group>
