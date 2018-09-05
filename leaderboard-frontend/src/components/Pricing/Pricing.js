d5f2936f23fa6d199cc32d40bb7a2eb701da8c65
cgoez <goez2001@gmail.com>
2018-09-05 16:44:22 -0700

    Pricing page initial commit

---
+++ leaderboard-frontend/src/components/Pricing/Pricing.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
+
+class Pricing extends Component {
+  constructor(props) {
+    super(props)
+    this.state = ''
+  }
+
+  render() {
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+}
