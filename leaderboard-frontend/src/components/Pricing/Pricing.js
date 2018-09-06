31411b20580309d4be0d9b81341b7aa54cbb3484
cgoez <goez2001@gmail.com>
2018-09-06 12:45:51 -0700

    Create css file, create CTA button for each plan

--- leaderboard-frontend/src/components/Pricing/Pricing.js
+++ leaderboard-frontend/src/components/Pricing/Pricing.js
@@ -9,6 +9,8 @@ import {
   Icon
 } from "semantic-ui-react";
 
+import "./Pricing.css";
+
 class Pricing extends Component {
   constructor(props) {
     super(props);
@@ -36,8 +38,7 @@ class Pricing extends Component {
           />
           <Header
             as="h2"
-            content="Pricing plans for everyone."
-            inverted
+            content="Plans to accomodate all class sizes."
             textAlign="center"
             style={{
               fontSize: "1.7em",
@@ -47,45 +48,50 @@ class Pricing extends Component {
           />
           <Grid container centered>
             <Grid.Column width={5}>
-              <Card>
-                <Card.Content header="FREE" textAlign="center" />
+              <Card className="Pricing__card">
+                <Card.Content as="h2" header="FREE" textAlign="center" />
                 <Card.Content>
                   <List divided relaxed size="huge">
                     <List.Item>
                       <List.Content>
-                        <List.Header as="h5">$0/month</List.Header>
+                        <List.Header as="h3" className=".Pricing__cardHeader">
+                          $0/month
+                        </List.Header>
                       </List.Content>
                     </List.Item>
                     <List.Item>
                       <List.Content>
                         <List.Description>
-                          <span style={{ fontWeight: "bold" }}>1</span> Admin
-                          User
+                          <strong>1</strong> Admin User
                         </List.Description>
                       </List.Content>
                     </List.Item>
                     <List.Item>
                       <List.Content>
                         <List.Description>
-                          <span style={{ fontWeight: "bold" }}>1</span> Class
+                          <strong>1</strong> Class
                         </List.Description>
                       </List.Content>
                     </List.Item>
                     <List.Item>
                       <List.Content>
                         <List.Description>
-                          <span style={{ fontWeight: "bold" }}>50</span>{" "}
-                          Students
+                          <strong>50</strong> Students
                         </List.Description>
                       </List.Content>
                     </List.Item>
                   </List>
                 </Card.Content>
-                <Card.Content extra>'Choose this plan'</Card.Content>
+                <Card.Content extra>
+                  <Button primary size="large" onClick={this.handleCTAClick}>
+                    Get Started
+                    <Icon name="right arrow" />
+                  </Button>
+                </Card.Content>
               </Card>
             </Grid.Column>
             <Grid.Column width={5}>
-              <Card>
+              <Card className="Pricing__card">
                 <Card.Content header="STANDARD" textAlign="center" />
                 <Card.Content>
                   <List divided relaxed size="huge">
@@ -96,7 +102,9 @@ class Pricing extends Component {
                     </List.Item>
                     <List.Item>
                       <List.Content>
-                        <List.Description>5 Admin Users</List.Description>
+                        <List.Description>
+                          <strong>5</strong> Admin Users
+                        </List.Description>
                       </List.Content>
                     </List.Item>
                     <List.Item>
@@ -111,11 +119,16 @@ class Pricing extends Component {
                     </List.Item>
                   </List>
                 </Card.Content>
-                <Card.Content extra>'Choose this plan'</Card.Content>
+                <Card.Content extra>
+                  <Button primary size="large" onClick={this.handleCTAClick}>
+                    Get Started
+                    <Icon name="right arrow" />
+                  </Button>
+                </Card.Content>
               </Card>
             </Grid.Column>
             <Grid.Column width={5}>
-              <Card>
+              <Card className="Pricing__card">
                 <Card.Content header="PREMIUM" textAlign="center" />
                 <Card.Content>
                   <List divided relaxed size="huge">
@@ -143,15 +156,14 @@ class Pricing extends Component {
                     </List.Item>
                   </List>
                 </Card.Content>
-                <Card.Content extra>'Choose this plan'</Card.Content>
+                <Card.Content extra>
+                  <Button primary size="large" onClick={this.handleCTAClick}>
+                    Get Started
+                    <Icon name="right arrow" />
+                  </Button>
+                </Card.Content>
               </Card>
             </Grid.Column>
-          </Grid>
-          <Grid centered style={{ paddingBottom: "2em", paddingTop: "2em" }}>
-            <Button primary size="huge" onClick={this.handleCTAClick}>
-              Get Started
-              <Icon name="right arrow" />
-            </Button>
           </Grid>
         </Container>
       </div>
